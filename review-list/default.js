b08cfa79e3eae2b37e036532fb7ce7fa8e15bcab
Zachary White <zfwhite91@gmail.com>
2016-05-31 11:28:09 -0700

    Added more images to restaurants. #2

--- review-list/default.js
+++ review-list/default.js
@@ -12,6 +12,7 @@ var reviews = [
     category: ['food', 'fish'],
     name: "Rubios",
     type: "restaurant",
+    image: 'images/rubios-logo.png',
     reviewer: 'Bill',
     review: 'Pretty good tacos, the fish was a little dry though.',
     score: 3
@@ -20,6 +21,7 @@ var reviews = [
     category: ['food', 'pizza'],
     name: "Flippin Pizza",
     type: "restaurant",
+    image: 'images/fpizza-logo.jpg',
     reviewer: 'Karly',
     review: 'Great pizza but the service left a bad taste in my mouth (unlike the pizza).',
     score: 3
@@ -28,6 +30,7 @@ var reviews = [
     category: ['food', 'fast'],
     name: "Dunkin' Donuts",
     type: "restaurant",
+    image: 'images/dunkindonuts-logo.gif',
     reviewer: 'Bill',
     review: 'Not the best but it is great when I am in a hurry on my way in to work.',
     score: 3
@@ -36,6 +39,7 @@ var reviews = [
     category: ['food', 'fish'],
     name: "Harbor Grill",
     type: "restaurant",
+    image: 'images/harborgrill-logo.jpg',
     reviewer: 'Ryan',
     review: 'A bit pricy but the food was absolutely delicious',
     score: 4
@@ -44,14 +48,17 @@ var reviews = [
     category: ['food', 'sandwiches'],
     name: "Board 'n Brew'",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Karen',
     review: 'Great beer selection, the sandwiches are just okay but the sweet and sour sauce is to die for!',
     score:4
   },
   {
+    //continue adding images here
     category: ['food', 'mexican'],
     name: "Taco Mesa",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Erica',
     review: 'Great tacos, the cashier was rude and it was definitely not easy on my wallet',
     score: 3
@@ -60,6 +67,7 @@ var reviews = [
     category: ['food', 'sandwiches'],
     name: "Checkers",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Karen',
     review: 'Will be back for more!',
     score: 5
@@ -68,6 +76,7 @@ var reviews = [
     category: ['food', 'mexican'],
     name: "La Salsa",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Alex',
     review: 'Quality salsa, subpar chicken',
     score: 3
@@ -76,6 +85,7 @@ var reviews = [
     category: ['food', 'chicken'],
     name: "Wing Stop",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Jeremy',
     review: 'Great wings!',
     score: 4
@@ -84,6 +94,7 @@ var reviews = [
     category: ['food', 'mexican'],
     name: "Chipotle",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Bill',
     review: 'The food was pretty good but they did not have carnitas!',
     score: 3
@@ -92,6 +103,7 @@ var reviews = [
     category: ['food', 'chinese'],
     name: "Panda Express",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Jay',
     review: 'You know what you are getting here.',
     score: 3
@@ -100,6 +112,7 @@ var reviews = [
     category: ['food', 'sandwiches'],
     name: "Subway",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Lisa',
     review: 'Fast but flavorless.',
     score: 2
@@ -108,6 +121,7 @@ var reviews = [
     category: ['food', 'sandwiches'],
     name: "Panera Bread",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Amy',
     review: 'Great soup, good sandwiches, will return!',
     score: 4
@@ -116,6 +130,7 @@ var reviews = [
     category: ['food', 'mexican'],
     name: "Oscars",
     type: "restaurant",
+    image: 'images/Wendys-logo.png',
     reviewer: 'Eric',
     review: 'Best fish tacos I have ever had!',
     score: 5
@@ -129,33 +144,6 @@ var searchButton = document.getElementById('search-button');
 var searchItem = document.getElementById('search-reviews');
 
 var body = document.getElementsByTagName('body')[0];
-
-//Search function
-/*
-function search(thing) {
-  var topDiv = document.createElement('div');
-  topDiv.setAttribute('id', 'top');
-  body.appendChild(topDiv);
-
-
-//append loop to use split and || to reference reviews categories in search
-  for (var i = 0; i < reviews.length; i++) {
-    if (reviews[i].name.toLowerCase().indexOf(thing.value.toLowerCase()) !== -1) {
-      var paragraph = document.createElement('p');
-      var div = document.createElement('div');
-      var header = document.createElement('h4');
-
-      div.setAttribute('class', 'myclass col-xs-8 col-xs-offset-2 text-center');
-      paragraph.setAttribute('class', 'reset');
-
-      header.textContent = reviews[i].name;
-      paragraph.textContent = reviews[i].reviewer + ": " + reviews[i].review + " ";
-      topDiv.appendChild(div);
-      div.appendChild(header);
-      div.appendChild(paragraph);
-    }
-  }
-}*/
 
 //function to create review boxes
 function reviewBox(reviewed) {
